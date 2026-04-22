bd1d9792701943cb8db3cd65899d7167ab0e3600
straiforos <stephen@traiforos.com>
2026-04-21 20:37:18 -0400

    fix macro registration for lingui.js

--- TaskManagement.Client/vite.config.ts
+++ TaskManagement.Client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['@lingui/macro'],
+        plugins: ['babel-plugin-macros'],
       },
     }),
     lingui(),
